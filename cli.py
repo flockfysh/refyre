4bd32c5b7d84be7da86d5154e3f5e13a153376c1
AnshKetchum <eye.am.ansh@gmail.com>
2023-05-28 13:47:58 -0700

    Fix cli entrypoint

--- cli.py
+++ cli.py
@@ -2,15 +2,16 @@ import argparse
 from refyre import Refyre
 
 #Instantiate an argument parser
-parser = argparse.ArgumentParser(
-    prog = "cli",
-    description = "refyre v.0.0.1 CLI",
-)
+def cli():
+    parser = argparse.ArgumentParser(
+        prog = "cli",
+        description = "refyre v.0.0.1 CLI",
+    )
 
-#Add positional arguments for input specs
-parser.add_argument("-i", '--input', help = "filepaths to the input specs, seperated by spaces", nargs='+')
-parser.add_argument("-o", '--output', help = "filepaths to the output specs, seperated by spaces", nargs='+')
+    #Add positional arguments for input specs
+    parser.add_argument("-i", '--input', help = "filepaths to the input specs, seperated by spaces", nargs='+')
+    parser.add_argument("-o", '--output', help = "filepaths to the output specs, seperated by spaces", nargs='+')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-ref = Refyre(input_specs = args.input, output_specs = args.output)
+    ref = Refyre(input_specs = args.input, output_specs = args.output)
