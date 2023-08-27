4e4509c22301a2fdc3812772f8b8a34070beb1d8
AnshKetchum <eye.am.ansh@gmail.com>
2023-08-27 16:48:31 -0700

    Add md extensions

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,11 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 
 # -- Options for HTML output -------------------------------------------------
