7add5da5bca155706d7670d15c1d6cef12caf756
AnshKetchum <eye.am.ansh@gmail.com>
2023-08-27 16:43:48 -0700

    Add md support for docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ extensions = [
     'sphinx.ext.autodoc', #Automatic documentation from docstrings
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
+    'myst_parser',
 ]
 
 templates_path = ['_templates']
